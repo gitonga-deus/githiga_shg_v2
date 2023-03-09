cef9c18e34f72de217a1af2c17140f2f3e7e08f1
gitonga <gitonga.deus@gmail.com>
2023-03-10 02:16:24 +0300

    feat: add website data

---
+++ config/data.jsx
@@ -0,0 +1,43 @@
+import { Values, Mission, Vision } from "@/components/Icons"
+
+export const HEADER_LINKS = [
+	{
+		href: "/",
+		text: "Home",
+	},
+	{
+		href: "/management",
+		text: "Management",
+	},
+	{
+		href: "/events",
+		text: "Events",
+	},
+	{
+		href: "/products",
+		text: "Products",
+	},
+	{
+		href: "/contact-us",
+		text: "Contact Us",
+	}
+]
+
+export const HERO_CARDS = [
+	{
+		icon: <Vision />,
+		title: "Vision",
+		description: "To be the preferred community-based financial service provider"
+	},
+	{
+		icon: <Mission />,
+		title: "Mission",
+		description: "To empower the members economically through saving mobilization, providing affordable credit, and prudent investment by applying transformative management styles."
+	},
+	{
+		icon: <Values />,
+		title: "Core Values",
+		description: "Accountability, Honesty, Integrity, Transparency, and Teamwork."
+	}
+]
+
