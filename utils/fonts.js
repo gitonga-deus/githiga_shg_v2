70805eefc98073254e59f6c4197760ec7f71ab7c
gitonga <gitonga.deus@gmail.com>
2023-03-10 02:23:38 +0300

    feat: add font imports (Inter)

---
+++ utils/fonts.js
@@ -0,0 +1,9 @@
+// ** Font Imports **
+import { Inter } from "next/font/google"
+
+export const textFont = Inter({
+	display: "swap",
+	subsets: ["latin"],
+	variable: "--font-inter",
+	weight: ["400", "500", "600", "700"],
+});
